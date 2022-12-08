c5fb6b323cd810aca510d9c01c69f1a2852da7a4
MarkJastineSiena <sienamarkjastine34@gmail.com>
2022-12-08 12:32:25 +0800

    this is for inheritance again

---
+++ merienda_function.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+/**
+ *
+ * @author Think pad
+ */
+public class merienda_function extends Additional_Functions{
+     //wala itong laman but for the sake ng inheritance kelangan ito ilagay
+   //lahat ng function ng additional_Function class ay kaya narin gawin nitong class nato.
+}
