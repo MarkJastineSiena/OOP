9c51d6c690c3dbc976631598752611ad4984d5e5
MarkJastineSiena <sienamarkjastine34@gmail.com>
2022-12-08 10:24:38 +0800

    this is for method overloading

---
+++ Additional_Functions.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+/**
+ *
+ * @author Think pad
+ */
+//method overloading
+public class Additional_Functions {
+    public int method(int a, int b){
+        return a * b;
+    }
+    public int method(int c, int d,int e){
+        return ((c * d) + 0);
+    }
+}
