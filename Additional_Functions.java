c46070fc9dbb8f8f3abb02d9f2d8d64a84727a46
MarkJastineSiena <101497565+MarkJastineSiena@users.noreply.github.com>
2022-12-10 14:26:53 +0800

    promo method added

--- Additional_Functions.java
+++ Additional_Functions.java
@@ -4,10 +4,8 @@
  */
 package project;
 
-/**
- *
- * @author Think pad
- */
+
+
 //method overloading
 public class Additional_Functions {
     public int method(int a, int b){
@@ -16,4 +14,16 @@ public class Additional_Functions {
     public int method(int c, int d,int e){
         return ((c * d) + 0);
     }
+    
+    //method for promo
+    public void promo() {
+        System.out.println("    Before this transaction ends we have a surprise for you");
+        System.out.println("    Since you purchace more than ₱80 you can choose 1 drink for free."+ '\n');
+        System.out.println("    Here is your choices:");
+        System.out.println("        1. Lemon Juice");
+        System.out.println("        2. Watermelon Soda");
+        System.out.println("        3. Cold Choco Milk");
+        System.out.println("        4. Soya Milk");
+        System.out.println("        5. Strawberry Juice "+ '\n');
+  }
 }
